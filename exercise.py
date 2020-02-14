0364a43fb55c8a808c7f55e37c8befef2cb1f1c7
Cassie <crs166@pitt.edu>
2020-02-14 16:28:01 -0500

    add exercise

---
+++ exercise.py
@@ -0,0 +1,5 @@
+name = input("Enter your name ")
+num1 = int(input("Enter an integer "))
+num2 = int(input("Enter another integer "))
+ans = num1**num2
+print(str(num1), "^", str(num2), "=", str(ans))
